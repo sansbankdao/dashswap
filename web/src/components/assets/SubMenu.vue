0a4faa267ce4a8b48aa2e39ca5f9e61ddffda99d
Shomari <nyusternie@sdot.io>
2025-05-29 18:29:32 -0400

    Update assets menu.

--- web/src/components/assets/SubMenu.vue
+++ web/src/components/assets/SubMenu.vue
@@ -1,14 +1,26 @@
 <template>
     <div>
-        <h2 class="text-sm font-medium text-gray-500">Pinned Projects</h2>
-        <ul role="list" class="mt-3 grid grid-cols-1 gap-5 sm:grid-cols-2 sm:gap-6 lg:grid-cols-4">
+        <h2 class="text-sm font-medium text-gray-500">
+            Assets Menu
+        </h2>
+
+        <ul role="list" class="mt-3 grid grid-cols-1 gap-5 sm:grid-cols-2 sm:gap-6">
             <li v-for="project in projects" :key="project.name" class="col-span-1 flex rounded-md shadow-sm">
-                <div :class="[project.bgColor, 'flex w-16 shrink-0 items-center justify-center rounded-l-md text-sm font-medium text-white']">{{ project.initials }}</div>
+                <div :class="[project.bgColor, 'flex w-16 shrink-0 items-center justify-center rounded-l-md text-sm font-medium text-white']">
+                    {{ project.initials }}
+                </div>
+
                 <div class="flex flex-1 items-center justify-between truncate rounded-r-md border-b border-r border-t border-gray-200 bg-white">
                     <div class="flex-1 truncate px-4 py-2 text-sm">
-                        <a :href="project.href" class="font-medium text-gray-900 hover:text-gray-600">{{ project.name }}</a>
-                        <p class="text-gray-500">{{ project.members }} Members</p>
+                        <a :href="project.href" class="font-medium text-gray-900 hover:text-gray-600">
+                            {{ project.name }}
+                        </a>
+
+                        <p class="text-gray-500">
+                            {{ project.members }} Members
+                        </p>
                     </div>
+
                     <div class="shrink-0 pr-2">
                         <button
                             type="button"
@@ -24,13 +36,13 @@
     </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { EllipsisVerticalIcon } from '@heroicons/vue/20/solid'
 
 const projects = [
-    { name: 'Graph API', initials: 'GA', href: '#', members: 16, bgColor: 'bg-pink-600' },
-    { name: 'Component Design', initials: 'CD', href: '#', members: 12, bgColor: 'bg-purple-600' },
-    { name: 'Templates', initials: 'T', href: '#', members: 16, bgColor: 'bg-yellow-500' },
-    { name: 'React Components', initials: 'RC', href: '#', members: 8, bgColor: 'bg-green-500' },
+    { name: 'Token Launcher', initials: 'GA', href: '#', members: 16, bgColor: 'bg-pink-600' },
+    { name: 'Airdrops!', initials: 'CD', href: '#', members: 12, bgColor: 'bg-purple-600' },
+    { name: 'TBD #1', initials: 'T', href: '#', members: 16, bgColor: 'bg-yellow-500' },
+    { name: 'TBD #2', initials: 'RC', href: '#', members: 8, bgColor: 'bg-green-500' },
 ]
 </script>
