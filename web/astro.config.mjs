11bd68b4ab5030790b64801a5546e014920e7644
Shomari <nyusternie@sdot.io>
2025-05-29 20:57:53 -0400

    Update config + layout.

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from '@astrojs/sitemap'
 
 export default defineConfig({
     output: 'static',
-    trailingSlash: 'always',
+    trailingSlash: 'never',
     site: 'https://devidev.io',
 
     // Single page, no prefetch needed
