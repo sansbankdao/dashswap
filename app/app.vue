d2b577a3b902639a16cccaa9c010fc37386ee130
Shomari <nyusternie@sdot.io>
2025-08-09 18:13:22 -0400

    Add meta tags.

--- app/app.vue
+++ app/app.vue
@@ -1,10 +1,8 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
     <section>
       <h1 class="text-5xl font-bold italic">
-        Tailwind
+        DashSwap
       </h1>
       <p class="text-2xl text-sky-400">
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis similique quis repellendus doloribus alias repudiandae! Explicabo aut praesentium illo vitae doloribus adipisci fuga velit officia? Sapiente repellat natus voluptatem autem?
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
+    ssr: false,
+
+    app: {
+        head: {
+            title: 'DashSwap',
+            meta: [
+                { name: 'description', content: '' },
+                { name: 'keywords', content: '' },
+            ],
+            link: [
+                { rel: 'canonical', href: 'https://dashswap.xyz' },
+                { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+            ],
+        },
+    },
+    modules: ['@nuxtjs/tailwindcss'],
+    devtools: { enabled: true },
+    compatibilityDate: '2025-07-15',
 })
