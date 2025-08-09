62b8ca5c2a07fc523f76b271fa5f74b6b76e0605
Shomari <nyusternie@sdot.io>
2025-08-09 17:16:55 -0400

    Debug import.

--- src/components/wallet/Dashboard.vue
+++ src/components/wallet/Dashboard.vue
@@ -2,21 +2,22 @@
     <main class="w-full flex flex-col gap-4">
         <section class="grid grid-cols-2 gap-3">
             <h2 class="col-span-2 text-2xl font-medium">
-                I Want to Receive ↴
+                THIS WALLET WILL BE GLORIOUS!!
             </h2>
         </section>
     </main>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { onMounted, ref } from 'vue'
 // import numeral from 'numeral'
+console.log('BEFORE IMPORT')
 import init, {
     WasmSdkBuilder,
     identity_fetch,
     prefetch_trusted_quorums_testnet,
 } from '../../libs/dash/wasm_sdk.js'
-
+console.log('AFTER IMPORT')
 /* Initialize stores. */
 // import { useSystemStore } from '@/stores/system'
 // const System = useSystemStore()
