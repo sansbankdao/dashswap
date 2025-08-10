37e8ac714e5ce6bbcb0db246760bf6d9d946b676
Shomari <nyusternie@sdot.io>
2025-08-09 22:27:18 -0400

    Enable WASM SDK.

--- pages/wallet.vue
+++ pages/wallet.vue
@@ -1,8 +1,14 @@
 <template>
-    <h1>
+    <h1 class="text-4xl text-amber-500">
         WALLET
     </h1>
-    <p>
+    <p class="text-slate-200">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique numquam modi labore corporis illo harum repudiandae reiciendis, dignissimos culpa animi. Iste ipsum doloribus sint doloremque nihil! Rerum aspernatur libero dolores.
     </p>
+
+    <WalletDashboard />
 </template>
+
+<script setup lang="ts">
+
+</script>
