74d0361ca045f6aed7e33564f40fface306a8095
Shomari <nyusternie@sdot.io>
2025-08-17 13:04:42 -0400

    Update help page.

---
+++ components/Help/Welcome.vue
@@ -0,0 +1,39 @@
+<template>
+    <main class="p-5 flex flex-col gap-6 items-center text-center sm:items-start sm:text-left">
+
+        <h1 class="text-sky-300 font-thin text-4xl">
+            Help &amp; Support
+        </h1>
+
+        <p class="text-slate-200">
+            Introducing the <span class="text-lg font-bold text-amber-300">Pre-Alpha Preview</span> for the soon to be released DashSwap decentralized exchange (DEX).
+        </p>
+
+        <section class="text-slate-200">
+            <h3 class="text-lg font-bold text-amber-300">
+                Features Services Include
+            </h3>
+
+            <ol class="pl-10 list-decimal">
+                <li>Instant $TOKEN swap</li>
+                <li>Custom $TOKEN launcher</li>
+                <li>Paymaster (for $DASH-free transfers)</li>
+            </ol>
+        </section>
+
+        <p class="text-slate-200">
+            DashSwap will offer full 1st-class support for all <span class="font-bold">DASH $TOKENS</span>; following the Dash Platform v2.0 upgrade and the introduction of <span class="font-bold">"Native" Fungible Tokens</span>.
+        </p>
+
+        <section class="text-slate-200">
+            <h3 class="text-lg font-bold text-amber-300">
+                Liquidity Providers (LPs)
+            </h3>
+
+            <p>
+                Work to GUARANTEE instant transaction matching, execution and settlement—assuring the community a healthy $TOKEN economy for both Builders and Creators alike.
+            </p>
+        </section>
+
+    </main>
+</template>
--- pages/help.vue
+++ pages/help.vue
@@ -2,20 +2,6 @@
     <main class="">
         <NavMenu />
 
-        <section class="p-5">
-            <h1 class="text-sky-200 text-6xl tracking-widest italic font-light">
-                Help &amp; Support
-            </h1>
-
-            <p class="text-sky-400 text-xl tracking-wider">
-                Chat in real-time with you frens and foes about "financial advice".
-            </p>
-        </section>
-
-        <section class="p-5">
-            <p class="text-amber-400 text-2xl tracking-tight">
-                Reach out directly to 0xShomari on X | @Shomari on Discord for help and support.
-            </p>
-        </section>
+        <HelpWelcome />
     </main>
 </template>
