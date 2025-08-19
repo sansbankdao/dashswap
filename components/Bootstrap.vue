29d5dae04a64faca60bdf908da90bd571c156f30
Shomari <nyusternie@sdot.io>
2025-08-19 16:39:42 -0400

    Update bootstrap onboarding.

--- components/Bootstrap.vue
+++ components/Bootstrap.vue
@@ -2,91 +2,118 @@
     <div class="bg-white py-6 sm:py-8 dark:bg-slate-900">
         <div class="mx-auto max-w-2xl px-6 lg:max-w-7xl lg:px-8">
             <div class="max-w-4xl">
-                <p class="text-xl font-semibold text-sky-600 dark:text-sky-400">
-                    Bootstrap the DASH Evolution
-                </p>
+                <h1 class="text-5xl font-semibold text-sky-500 dark:text-sky-300">
+                    Got <span class="text-6xl text-rose-500 dark:text-rose-300">$AN$</span> FOMO?
+                </h1>
+
+                <h2 class="text-2xl font-semibold text-sky-600 dark:text-sky-400">
+                    Don't worry! We've got you covered.
+                </h2>
 
                 <h1 class="mt-2 text-pretty text-4xl font-semibold tracking-tight text-slate-900 sm:text-5xl dark:text-white">
                     Sansbank is on a mission to empower bank-free sovereignty
                 </h1>
 
                 <p class="mt-6 text-balance text-xl/8 text-slate-700 dark:text-slate-300">
-                    We belive that everyone should have the option to remain financial sovereign.
-
-                    At arcu, sit dui mi, nibh dui, diam eget aliquam.
-                    Quisque id at vitae feugiat egestas.
+                    We belive that everyone should have the option to remain financial sovereign
+                    — we're here to make sure that EVERYONE who shows up EARLY gets to EAT!
                 </p>
             </div>
 
             <section class="mt-10 grid grid-cols-1 lg:grid-cols-2 lg:gap-x-8 lg:gap-y-8">
-                <div class="lg:pr-8">
+                <div class="lg:pr-8 space-y-6">
                     <h2 class="text-pretty text-4xl font-semibold tracking-tight text-slate-900 dark:text-white">
                         Our Vision
                     </h2>
 
-                    <p class="mt-6 text-base/7 text-slate-600 dark:text-slate-400">
-                        Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque erat velit. Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis id.
+                    <p class="text-base/7 text-slate-600 dark:text-slate-200">
+                        Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor praesent donec est.
                     </p>
 
-                    <p class="mt-8 text-base/7 text-slate-600 dark:text-slate-400">
-                        Et vitae blandit facilisi magna lacus commodo. Vitae sapien duis odio id et. Id blandit molestie auctor fermentum dignissim. Lacus diam tincidunt ac cursus in vel. Mauris varius vulputate et ultrices hac adipiscing egestas. Iaculis convallis ac tempor et ut. Ac lorem vel integer orci.
+                    <h2 class="text-pretty text-4xl font-semibold tracking-tight text-slate-900 dark:text-white">
+                        Why Sansnote?
+                    </h2>
+
+                    <p class="text-base/7 text-slate-600 dark:text-slate-200">
+                        Governance gives everyone a voice, which is the 1st rule of building a successful community.
                     </p>
                 </div>
 
                 <div class="pt-32 lg:row-span-2 lg:-mr-16 xl:mr-auto">
                     <div class="-mx-8 grid grid-cols-2 gap-4 sm:-mx-16 sm:grid-cols-4 lg:mx-0 lg:grid-cols-2 xl:gap-8">
                         <div class="aspect-square overflow-hidden rounded-xl shadow-xl outline outline-1 -outline-offset-1 outline-black/10 dark:shadow-none dark:outline-white/10">
-                            <img alt="" src="https://images.unsplash.com/photo-1590650516494-0c8e4a4dd67e?&auto=format&fit=crop&crop=center&w=560&h=560&q=90" class="block size-full object-cover" />
+                            <img alt="" src="https://images.unsplash.com/photo-1509017174183-0b7e0278f1ec?q=80" class="block size-full object-cover" />
                         </div>
 
                         <div class="-mt-8 aspect-square overflow-hidden rounded-xl shadow-xl outline outline-1 -outline-offset-1 outline-black/10 lg:-mt-40 dark:shadow-none dark:outline-white/10">
-                            <img alt="" src="https://images.unsplash.com/photo-1557804506-669a67965ba0?&auto=format&fit=crop&crop=left&w=560&h=560&q=90" class="block size-full object-cover" />
+                            <img alt="" src="https://plus.unsplash.com/premium_photo-1681928677639-44267dea6af7?q=80" class="block size-full object-cover" />
                         </div>
 
                         <div class="aspect-square overflow-hidden rounded-xl shadow-xl outline outline-1 -outline-offset-1 outline-black/10 dark:shadow-none dark:outline-white/10">
-                            <img alt="" src="https://images.unsplash.com/photo-1559136555-9303baea8ebd?&auto=format&fit=crop&crop=left&w=560&h=560&q=90" class="block size-full object-cover" />
+                            <img alt="" src="https://images.unsplash.com/photo-1706827183359-6b015aaad56c?q=80" class="block size-full object-cover" />
                         </div>
 
                         <div class="-mt-8 aspect-square overflow-hidden rounded-xl shadow-xl outline outline-1 -outline-offset-1 outline-black/10 lg:-mt-40 dark:shadow-none dark:outline-white/10">
-                            <img alt="" src="https://images.unsplash.com/photo-1598257006458-087169a1f08d?&auto=format&fit=crop&crop=center&w=560&h=560&q=90" class="block size-full object-cover" />
+                            <img alt="" src="https://images.unsplash.com/photo-1750323844552-892fd931894b?q=80" class="block size-full object-cover" />
                         </div>
                     </div>
                 </div>
 
                 <div class="max-lg:mt-16 lg:col-span-1">
                     <p class="text-3xl font-semibold text-slate-500 dark:text-slate-200">
-                        DashSwap 2025 Goals
+                        DashSwap 2025 KPI Goals
                     </p>
 
                     <hr class="mt-6 border-t border-slate-200 dark:border-slate-700" />
 
                     <dl class="mt-6 grid grid-cols-1 gap-x-8 gap-y-4 sm:grid-cols-2">
                         <div class="flex flex-col gap-y-2 border-b border-dotted border-slate-200 pb-4 dark:border-slate-700">
-                            <dt class="text-sm/6 text-slate-600 dark:text-slate-400">
-                                Total Swap Value
+                            <dt class="text-sm/6 text-slate-600 dark:text-slate-200">
+                                Tokens Launched
                             </dt>
 
                             <dd class="order-first text-6xl font-semibold tracking-tight text-slate-900 dark:text-white">
-                                $<span>1</span>M
+                                <span>15</span>K
                             </dd>
                         </div>
 
                         <div class="flex flex-col gap-y-2 border-b border-dotted border-slate-200 pb-4 dark:border-slate-700">
-                            <dt class="text-sm/6 text-slate-600 dark:text-slate-400">Companies</dt>
-                            <dd class="order-first text-6xl font-semibold tracking-tight text-slate-900 dark:text-white"><span>30</span>K</dd>
+                            <dt class="text-sm/6 text-slate-600 dark:text-slate-200">
+                                Total Swap Value
+                            </dt>
+
+                            <dd class="order-first text-6xl font-semibold tracking-tight text-slate-900 dark:text-white">
+                                $<span>3</span>M
+                            </dd>
                         </div>
 
                         <div class="flex flex-col gap-y-2 max-sm:border-b max-sm:border-dotted max-sm:border-slate-200 max-sm:pb-4 dark:max-sm:border-slate-700">
-                            <dt class="text-sm/6 text-slate-600 dark:text-slate-400">Deals Closed</dt>
-                            <dd class="order-first text-6xl font-semibold tracking-tight text-slate-900 dark:text-white"><span>1.5</span>M</dd>
+                            <dt class="text-sm/6 text-slate-600 dark:text-slate-200">
+                                SANS Staked
+                            </dt>
+
+                            <dd class="order-first text-6xl font-semibold tracking-tight text-slate-900 dark:text-white">
+                                <span>150</span>K
+                            </dd>
                         </div>
 
                         <div class="flex flex-col gap-y-2">
-                            <dt class="text-sm/6 text-slate-600 dark:text-slate-400">Leads Generated</dt>
-                            <dd class="order-first text-6xl font-semibold tracking-tight text-slate-900 dark:text-white"><span>200</span>M</dd>
+                            <dt class="text-sm/6 text-slate-600 dark:text-slate-200">
+                                SANS Stake Payouts
+                            </dt>
+
+                            <dd class="order-first text-6xl font-semibold tracking-tight text-slate-900 dark:text-white">
+                                <span>$7.5</span>K
+                            </dd>
                         </div>
                     </dl>
                 </div>
+            </section>
+
+            <section class="mt-10 px-10 py-5">
+                <a href="https://sansbank.org/bootstrap" target="_blank" class="px-5 py-3 text-5xl font-bold text-center text-sky-800 bg-sky-200 border-4 border-sky-400 rounded-2xl hover:bg-sky-800 hover:text-sky-100">
+                    Get More $AN$
+                </a>
             </section>
         </div>
     </div>
