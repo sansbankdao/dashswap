e427b5f870cb5a66306ece1487eccbc25f1a1a72
Shomari <nyusternie@sdot.io>
2025-08-19 16:56:19 -0400

    Update bootstrap verbiage.

--- components/Bootstrap.vue
+++ components/Bootstrap.vue
@@ -2,40 +2,33 @@
     <div class="bg-white py-6 sm:py-8 dark:bg-slate-900">
         <div class="mx-auto max-w-2xl px-6 lg:max-w-7xl lg:px-8">
             <div class="max-w-4xl">
-                <h1 class="text-5xl font-semibold text-sky-500 dark:text-sky-300">
-                    Got <span class="text-6xl text-rose-500 dark:text-rose-300">$AN$</span> FOMO?
+                <h1 class="text-6xl font-semibold text-sky-500 dark:text-sky-300">
+                    Got <span class="text-7xl text-rose-500 dark:text-rose-300">$AN$</span> FOMO?
                 </h1>
 
-                <h2 class="text-2xl font-semibold text-sky-600 dark:text-sky-400">
-                    Don't worry! We've got you covered.
+                <h2 class="pl-0.5 text-3xl font-semibold text-sky-600 dark:text-sky-400">
+                    Don't worry! We've got you covered
                 </h2>
 
-                <h1 class="mt-2 text-pretty text-4xl font-semibold tracking-tight text-slate-900 sm:text-5xl dark:text-white">
-                    Sansbank is on a mission to empower bank-free sovereignty
+                <h1 class="mt-5 text-pretty text-4xl font-semibold tracking-tight text-slate-900 sm:text-5xl dark:text-white">
+                    Sansbank is building DashSwap with YOUR ❤️ support
                 </h1>
 
                 <p class="mt-6 text-balance text-xl/8 text-slate-700 dark:text-slate-300">
-                    We belive that everyone should have the option to remain financial sovereign
-                    — we're here to make sure that EVERYONE who shows up EARLY gets to EAT!
+                    We believe that everyone should have a clear path to financial sovereignty
+                    — we're here to make sure that EVERYONE in the Dash community who shows up EARLY to Sansbank gets to EAT!
                 </p>
             </div>
 
             <section class="mt-10 grid grid-cols-1 lg:grid-cols-2 lg:gap-x-8 lg:gap-y-8">
                 <div class="lg:pr-8 space-y-6">
-                    <h2 class="text-pretty text-4xl font-semibold tracking-tight text-slate-900 dark:text-white">
+                    <h2 class="text-pretty text-4xl font-semibold tracking-tight text-amber-700 dark:text-amber-200">
                         Our Vision
                     </h2>
 
                     <p class="text-base/7 text-slate-600 dark:text-slate-200">
-                        Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor praesent donec est.
-                    </p>
-
-                    <h2 class="text-pretty text-4xl font-semibold tracking-tight text-slate-900 dark:text-white">
-                        Why Sansnote?
-                    </h2>
-
-                    <p class="text-base/7 text-slate-600 dark:text-slate-200">
-                        Governance gives everyone a voice, which is the 1st rule of building a successful community.
+                        Provide a seemless experience for both users and builders to trade Dash assets instantly
+                        — we aim to support the monetization of ideas through the responsible use of utility tokens.
                     </p>
                 </div>
 
@@ -60,9 +53,9 @@
                 </div>
 
                 <div class="max-lg:mt-16 lg:col-span-1">
-                    <p class="text-3xl font-semibold text-slate-500 dark:text-slate-200">
-                        DashSwap 2025 KPI Goals
-                    </p>
+                    <h2 class="text-pretty text-4xl font-semibold tracking-tight text-amber-700 dark:text-amber-200">
+                        2025 KPI Goals
+                    </h2>
 
                     <hr class="mt-6 border-t border-slate-200 dark:border-slate-700" />
 
@@ -108,11 +101,24 @@
                         </div>
                     </dl>
                 </div>
+
+                <div class="lg:pr-8 space-y-6">
+                    <h2 class="text-pretty text-4xl font-semibold tracking-tight text-amber-700 dark:text-amber-200">
+                        Why Sansnote?
+                    </h2>
+
+                    <p class="text-base/7 text-slate-600 dark:text-slate-200">
+                        An open &amp; transparent governance system gives everyone a voice in shaping the direction of DashSwap, which is the 1st rule in building a successful community.
+                    </p>
+                </div>
             </section>
 
             <section class="mt-10 px-10 py-5">
-                <a href="https://sansbank.org/bootstrap" target="_blank" class="px-5 py-3 text-5xl font-bold text-center text-sky-800 bg-sky-200 border-4 border-sky-400 rounded-2xl hover:bg-sky-800 hover:text-sky-100">
+                <a href="https://sansbank.org/bootstrap" target="_blank" class="px-5 py-3 flex flex-col items-center text-5xl font-bold text-center text-sky-800 bg-sky-200 border-4 border-sky-400 rounded-2xl hover:bg-sky-800 hover:text-sky-100">
                     Get More $AN$
+                    <span class="block text-xs uppercase">
+                        This Is Not Financial Advice
+                    </span>
                 </a>
             </section>
         </div>
