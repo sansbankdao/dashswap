a1c6542fa671099cc4469f2a15d8de5305049eaf
Shomari <nyusternie@sdot.io>
2025-08-24 16:29:09 -0400

    Add claim page.

---
+++ pages/claim.vue
@@ -0,0 +1,34 @@
+<template>
+    <section class="flex flex-col gap-3 p-5">
+        <h1 class="text-sky-200 text-6xl tracking-widest italic font-light">
+            Claim My Username Assistant
+        </h1>
+
+        <p class="text-sky-400 text-xl tracking-wider">
+            Are you ready to preserve your space of the DASH Evolution?
+        </p>
+
+        <p class="text-sky-400 text-xl tracking-wider">
+            This guide was prepared especially for iOS users whom currently DO NOT have access to DashPay.
+        </p>
+    </section>
+</template>
+
+<script setup lang="ts">
+/* Import modules. */
+import moment from 'moment'
+
+useHead({
+    title: 'Claim My Username Assistant by DashSwap'
+})
+
+// onMounted(() => {
+//     console.log('Mounted!')
+//     // Now it's safe to perform setup operations.
+// })
+
+// onBeforeUnmount(() => {
+//     console.log('Before Unmount!')
+//     // Now is the time to perform all cleanup operations.
+// })
+</script>
