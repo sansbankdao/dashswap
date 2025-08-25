f2bd91facc1b65456019714c88a32e8730143f9f
Shomari <nyusternie@sdot.io>
2025-08-25 14:05:49 -0400

    Update roadmap.

--- pages/roadmap.vue
+++ pages/roadmap.vue
@@ -9,9 +9,23 @@
         </p>
 
         <ol class="pl-10 pt-5 list-decimal text-slate-200 text-2xl font-bold space-y-3">
+            <li>📌 Claim username assistant</li>
+            <li>📌 Rainmaker</li>
+            <li>📌 Asset gallery</li>
+            <li>📌 Token launcher</li>
+            <li>📌 Identity manager</li>
+            <li>📌 Profile manager</li>
+            <li>📌 Asset swap</li>
             <li>📌 Chrome browser extension integration</li>
             <li>📌 Ledger HW support</li>
             <li>📌 Trezor/OneKey HW support</li>
+            <li>📌 Trollbox</li>
+            <li>📌 PRO subscriptions</li>
+            <li>📌 Paymaster</li>
+            <li>📌 Leaderboard</li>
+            <li>📌 Help &amp; support</li>
+            <li>📌 Front-end frameworks</li>
+            <li>📌 Back-end frameworks</li>
             <li>✅ Bip-34 (seed phrase) support</li>
             <li>✅ Self-custodial Platform web wallet</li>
         </ol>
